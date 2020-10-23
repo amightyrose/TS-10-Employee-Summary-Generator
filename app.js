a07520b956da9314d7095d4a1d58a3ae801c9085
timsilby <69242373+timsilby@users.noreply.github.com>
2020-10-23 19:12:33 +1000

    Added userDialogs to lib and empty promptUser file. Added init function to app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+const userDialogs = require('./lib/userDialogs')
+const promptUser = require('./lib/promptUser');
+const render = require("./lib/htmlRenderer");
+// const generateMarkdown = require('./lib/generateMarkdown');
+const writeFileAsync = util.promisify(fs.writeFile);
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -33,3 +35,36 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+
+async function init() {
+
+	try {
+
+		userDialogs.showWelcome();
+
+		if (!(await userDialogs.continuePrompt()).continue) {return};
+
+		// const arrEmployees = await promptUser();
+
+		// const markup = generateMarkdown(answers);
+
+		// userDialogs.previewMD(md);
+
+		// if (!(await userDialogs.continuePrompt()).continue) {return};
+
+		// await writeFileAsync("./output/README.md", md)
+
+		// userDialogs.showSuccessMsg();
+
+	}
+	catch(err) {
+
+		console.log(`The following error was encountered: ${err}`);
+
+	}
+
+}
+
+
+init();
